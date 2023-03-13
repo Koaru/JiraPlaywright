00cb0ed79cc55221e9bcf2ab5366e10e98bd90a8
S H <mucsanyi.b@gmail.com>
2023-03-13 14:07:09 +0100

    logout test cases

--- pages/dashboardPage.ts
+++ pages/dashboardPage.ts
@@ -11,4 +11,12 @@ export default class DashboardPage{
     async clickOnViewProfile(){
         await this.page.click("#view_profile");
     }
+
+    async logoutMessageIsPresent(){
+        return this.page.locator("#//div[@class='aui-message aui-message-info']").isVisible;
+    }
+
+    async clickOnLogoutBtn(){
+        await this.page.click("#log_out");
+    }
 }
---
+++ tests/logout.test.ts
@@ -0,0 +1,16 @@
+import {test, expect} from "@playwright/test";
+import LoginPage from "../pages/loginPage";
+import DashboardPage from "../pages/dashboardPage";
+
+const USERNAME = "automation40";
+const PASSWORD = "CCAutoTest19.";
+
+
+test("Logout", async ({page}) => {
+    const login = new LoginPage(page);
+    const dashboard = new DashboardPage(page);
+    await login.login(USERNAME,PASSWORD);
+    dashboard.clickOnProfile;
+    dashboard.clickOnLogoutBtn;
+    expect(dashboard.logoutMessageIsPresent).toBeTruthy;
+})
