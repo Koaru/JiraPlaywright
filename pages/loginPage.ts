5630b6a8f102bc553a017509bff10b69648b0906
S H <mucsanyi.b@gmail.com>
2023-03-13 13:55:35 +0100

    login test cases

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -19,4 +19,15 @@ export default class LoginPage{
     async navigateToLoginPage(){
         await this.page.goto("https://jira-auto.codecool.metastage.net/secure/Dashboard.jspa")
     }
+
+    async errorMessageIsPresent(){
+        return this.page.locator("#usernameerror").isVisible;
+    }
+
+    async login(username: string, pwd: string){
+        await this.navigateToLoginPage();
+        await this.enterUserName(username);
+        await this.enterPassword(pwd);
+        await this.clickLogIn();
+    }
 }
--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,21 +1,34 @@
 import {test, expect} from "@playwright/test";
 import LoginPage from "../pages/loginPage";
 import DashboardPage from "../pages/dashboardPage";
-import ProfilePage from "../pages/profilePage";
 
 const USERNAME = "automation40";
+const INVALIDUSERNAME = "invalidusername";
 const PASSWORD = "CCAutoTest19.";
+const INVALIDPASSWORD = "invalidpassword";
 
-test("Happy Path login",async ({page}) => {
+
+test("Happy Path login", async ({page}) => {
     const login = new LoginPage(page);
     const dashboard = new DashboardPage(page);
-    const profilePage = new ProfilePage(page);
-    await login.navigateToLoginPage();
-    await login.enterUserName(USERNAME);
-    await login.enterPassword(PASSWORD);
-    await login.clickLogIn();
+    await login.login(USERNAME,PASSWORD);
     await dashboard.clickOnProfile();
     await dashboard.clickOnViewProfile();
     let userName = await page.textContent("#up-d-username");
-    expect(userName).toContain(USERNAME);
+    expect(userName).toContain(USERNAME); 
+})
+
+
+test("Invalid username", async ({page}) => {
+    const login = new LoginPage(page);
+    await login.login(INVALIDUSERNAME,PASSWORD);
+    console.log(login.errorMessageIsPresent);
+    expect(login.errorMessageIsPresent).toBeTruthy();
+})
+
+test("Invalid password", async ({page}) => {
+    const login = new LoginPage(page);
+    await login.login(USERNAME,INVALIDPASSWORD);
+    console.log(login.errorMessageIsPresent);
+    expect(login.errorMessageIsPresent).toBeTruthy();
 })
